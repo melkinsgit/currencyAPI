d2d90155289887e77f18fb81357d968861520a07
melkinsgit <margaret.elkins@gmail.com>
2016-03-15 19:31:25 -0500

    Working on API

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,7 @@
 var express = require("express");
 var router = express.Router();
 
-var request = require('request');
-
-var myNewURL = "http://www.apilayer.net/api/live?access_key=35e392214ea77ebc196ea8b070a60eb8&currencies=USD,EUR,GBP&format=1";
-
-request(myNewURL, function (error, response, body){  // request is my req var id'd above; reponse is what the API gives me back, body is my data
-    console.log(body);
-    var convertObject = JSON.parse(body);  // body is string returned, JSON parses into an object
-    var quotes = convertObject.quotes.USDEUR;
-    console.log(quotes);
-});
+var request = require('request');  // this enables the program to send a request to an API
 
 /* GET the app's home page; router method */
 router.get('/', homepage);
@@ -24,6 +15,16 @@ router.get('/convert', convert);
 
 function convert(req, res) {
 
+	var myNewURL = "http://www.apilayer.net/api/live?access_key=35e392214ea77ebc196ea8b070a60eb8&currencies=EUR,GBP&format=1";
+
+	var convertObject;
+
+	request(myNewURL, function (error, response, body){  // request is my req var id'd above; reponse is what the API gives me back, body is my data
+		console.log(body);
+		convertObject = JSON.parse(body);  // body is string returned, JSON parses into an object
+		//quotes = convertObject;
+		console.log(convertObject.quotes.USDGBP);
+
 	// get all vars from index.jade input fields and dropdowns
 	var amountToConvert = req.query.convertAmount;
 	var convertFrom = req.query.fromCurrency;
@@ -31,15 +32,19 @@ function convert(req, res) {
 	var convertedVal;  // declare for access in all blocks below
 	var errorString = '';  // declare and initialize as empty string, will only change if necessary
 
-	// CLARA & MARGARET npm install request --save so i can send an API request; --save adds version to package.json - user's machine from git loads all correct versions - NPM for node module pieces
-
-
+	// CLARA & MARGARET npm install request --save so I can send an API request; --save adds version to package.json - user's machine from git loads all correct versions - NPM for node module pieces
 
 	//console.log("query was: convert " + dollars + " to " + convertTo);
 
+
+	console.log("about to use converter " + convertObject.quotes.USDGBP);
+
 	// used amounts from web
-	var dollarConversions = {"Pounds": 0.7, "Euros": 0.9};
-	var poundToEuro = 1.3;
+	var usdToGBP = convertObject.quotes.USDGBP;
+	var usdToEUR = convertObject.quotes.USDEUR;
+	console.log('the two conversion rates are ' + usdToGBP + ' and ' + usdToEUR);
+	var dollarConversions = {"Pounds": usdToGBP, "Euros": usdToEUR};
+	var poundToEuro = usdToGBP / usdToGBP;
 
 	// get conversion rates from currency layer
 	//CL.source('USD').currencies('EUR').live(function(result) {
@@ -48,7 +53,7 @@ function convert(req, res) {
 
 	// response if convert from and to currencies are the same
 	if (convertFrom === convertTo) {
-		errorString = 'You must have different values for Convert From and Convert To. Please go back to the previous page and try again.'
+		errorString = 'You must have different values for Convert From and Convert To. Please go back to the previous page and try again.';
 		res.render('result', {
 			errorMsg: errorString,
 			amount: amountToConvert,
@@ -102,6 +107,8 @@ function convert(req, res) {
 			toConverted: convertedVal
 		});
 	}
+
+	});
 }
 
 module.exports = router;
